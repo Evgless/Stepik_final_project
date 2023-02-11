4df49e3c6c30916cf08c30556a28aa89160f331c
Evgless <evgless@mail.ru>
2023-02-11 19:34:56 +0300

    Refactor BasePage and MainPage

--- conftest.py
+++ conftest.py
@@ -28,5 +28,5 @@ def browser(request):
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
-    # time.sleep(5)
+    time.sleep(5)
     browser.quit()
--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,10 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     ADD_TO_CART = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
 
-class ProductPageLocators():
+class ProductPageLocators:
     ADD_TO_CART_BTN = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
     PRODUCT_NAME = (By.CSS_SELECTOR, "div.col-sm-6.product_main > h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, "div.col-sm-6.product_main > p.price_color")
@@ -12,6 +12,10 @@ class ProductPageLocators():
     PRODUCT_PRICE_CHECK = (By.CSS_SELECTOR, "div#messages > .alert.alert-safe.alert-noicon.alert-info.fade.in strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div#messages > div.alert.alert-safe.alert-noicon.alert-success.fade.in:nth-child(1)")
 
-class LoginPageLocators():
+class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
